22ed3e6b8e8e5ba689d7bcd6edc5bb3757caa105
Vusi Mosena <vusi@evotree.co.za>
2019-03-31 23:10:55 +0200

    Added Basic filter on sample data

--- functions/graphql/resolvers/index.js
+++ functions/graphql/resolvers/index.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-
+const lodash = require('lodash');
 const authors = [
 	{ id: 1, firstName: 'Tom', lastName: 'Coleman' },
 	{ id: 2, firstName: 'Sashko', lastName: 'Stubailo' }
@@ -9,6 +9,11 @@ const posts = [
 	{ id: 1, authorId: 1, title: 'Introduction to GraphQL', votes: 2 },
 	{ id: 2, authorId: 2, title: 'GraphQL Rocks', votes: 3 },
 	{ id: 3, authorId: 2, title: 'Advanced GraphQL', votes: 1 }
+];
+
+const items = [
+	{ id: 1, name: 'IceCream 1', description: 'Introduction to GraphQL', rating: 1.2 },
+	{ id: 3, name: 'IceCream 3', description: 'Introduction to GraphQL', rating: 1.5 }
 ];
 
 const resolveFunctions = {
@@ -29,6 +34,29 @@ const resolveFunctions = {
 					return { code: '400', status: 'Failure', message: 'No record(s) found.' };
 				}
 			});
+		},
+		listIceCreams(_, { filter, limit, nextToken }) {
+			const searchField = filter.searchField.contains;
+			return {
+				items: lodash.filter(items, function(item) {
+					var tempField = item.name + ' ' + item.description;
+					return tempField.includes(searchField);
+				}),
+				nextToken: searchField
+			};
+		},
+		getIceCream(_, { id }) {
+			return items.find((item) => item.id === id);
+		},
+
+		searchPosts(_, { filter, limit, nextToken }) {
+			return {
+				posts: lodash.filter(posts, function(post) {
+					var tempField = post.title;
+					return tempField.includes(filter);
+				}),
+				nextToken: filter
+			};
 		}
 	},
 	Mutation: {
@@ -40,7 +68,23 @@ const resolveFunctions = {
 			post.votes += 1;
 			// pubsub.publish('postUpvoted', post);
 			return post;
+		},
+		createIceCream(_, { input }) {
+			var vars = input;
+			var s1 = input.name.toLowerCase();
+			var s2 = input.description.toLowerCase();
+			vars.searchField = '${s1} ${s2}';
+			items.push({
+				id: Math.round(Math.random() * 100),
+				...vars
+			});
+			return 'Addded Successfully!';
 		}
+
+		// listUsers(parent, { filter: { searchField } }) {
+		// 	console.log(parent);
+		// 	return searchField;
+		// }
 	},
 	Author: {
 		posts(author) {
--- functions/graphql/schema/index.js
+++ functions/graphql/schema/index.js
@@ -2,6 +2,66 @@ const { ApolloServer, gql } = require('apollo-server-express');
 const resolvers = require('../resolvers');
 
 const typeDefs = gql`
+	# ---------------- Example for Search Demo ----------------------
+	type IceCream {
+		id: Int!
+		name: String!
+		description: String!
+		rating: Float
+		searchField: String
+	}
+
+	input CreateIceCreamInput {
+		name: String!
+		description: String!
+		rating: Float
+	}
+
+	input TableIceCreamFilterInput {
+		id: TableIDFilterInput
+		name: TableStringFilterInput
+		description: TableStringFilterInput
+		rating: TableFloatFilterInput
+		searchField: TableStringFilterInput
+	}
+
+	input TableIDFilterInput {
+		id: Int!
+	}
+
+	input TableFloatFilterInput {
+		rating: Float!
+	}
+
+	input TableStringFilterInput {
+		ne: String
+		eq: String
+		le: String
+		lt: String
+		ge: String
+		gt: String
+		contains: String
+		notContains: String
+		between: [String]
+		beginsWith: String
+	}
+
+	type IceCreamConnection {
+		items: [IceCream]!
+		nextToken: String
+	}
+
+	# mutation create {
+	# 	createIceCream(input: {
+	# 		name: "Peanut Butter World"
+	# 		description: "Milk chocolate ice cream with peanut buttery swirls & chocolate cookie swirls"
+	# 	}) {
+	# 		name
+	# 	}
+	# }
+
+	# ----------------------------------
+
 	type Author {
 		id: Int! # the ! means that every author object _must_ have an id
 		firstName: String
@@ -17,11 +77,14 @@ const typeDefs = gql`
 	}
 
 	type User {
-		userID: String
 		email: String
 		firstName: String
 		lastName: String
-		photoURL: String
+	}
+
+	type SearchPosts {
+		posts: [Post]
+		nexToken: String
 	}
 
 	# the schema allows the following query:
@@ -29,12 +92,42 @@ const typeDefs = gql`
 		posts: [Post]
 		author(id: Int!): Author
 		users: [User]
+		searchPosts(filter: String, limit: Int, nextToken: String): SearchPosts
+		getIceCream(id: Int): IceCream
+		listIceCreams(filter: TableIceCreamFilterInput, limit: Int, nextToken: String): IceCreamConnection
 	}
 
 	# this schema allows the following mutation:
 	type Mutation {
 		upvotePost(postId: Int!): Post
+		#listUsers(filter: TableUserFilterInput, limit: Int, nextToken: String): UserResults
+		createIceCream(input: CreateIceCreamInput): IceCream
 	}
+	#  IceCreamConnection.items must be Input Type but got: [IceCream]
+	# input UserResults {
+	# 	user: TableUserFilterInput
+	# 	nextToken: String
+	# }
+
+	# input TableUserFilterInput {
+	# 	firstName: TableStringFilterInput
+	# 	lastName: TableStringFilterInput
+	# 	email: TableStringFilterInput
+	# 	searchField: TableStringFilterInput
+	# }
+
+	# input TableStringFilterInput {
+	# 	ne: String
+	# 	eq: String
+	# 	le: String
+	# 	lt: String
+	# 	ge: String
+	# 	gt: String
+	# 	contains: String
+	# 	notContains: String
+	# 	between: [String]
+	# 	beginsWith: String
+	# }
 `;
 const graphqlServer = new ApolloServer({
 	typeDefs,
